2cfda256b8d82b8de51359c000024ec3e5ab3d9d
RickOwri <mickael.rutembya@gmail.com>
2023-05-29 01:56:13 +0200

    backendapp

---
+++ src/app.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Param, Post, Body, Query, ForbiddenException } from '@nestjs/common';
+import { AppService } from './app.service';
+import { RequestTokenDto } from './dtos/requestToken.dto';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('last-block')
+  getLastBlock() {
+    return this.appService.getLastBlock();
+  }
+
+  @Get('contract-address')
+  getContractAddress(){
+    return this.appService.getContractAddress();
+  }
+
+  @Get('total-supply')
+  geTotalSupply() {
+    return this.appService.getTotalSupply();
+  }
+
+  @Get('balance/address')
+  getBalance(@Param('address') address: string) {
+    return this.appService.getBalance(address)
+  }
+
+  @Get("receipt")
+  getReceipt(@Query('hash') hash: string) {
+    this.appService.getReceipt(hash);
+  }
+
+  a: number = 0;
+
+  @Post('request-tokens')
+  requestTokens(@Body() body: RequestTokenDto) {
+    return this.appService.requestTokens(body.address);
+  }
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import {ethers} from 'ethers';
+import { ConfigService } from '@nestjs/config';
+import * as tokenJson from './assets/MyToken.json';
+
+
+const contractAddressToken = "0x35a24F28f846DB57F13B534799659824A81f31FF"
+
+const contractAddressBallot = "0x9267611856783B54c2d296F3594519bb37BFc63c"
+@Injectable()
+export class AppService {
+  provider: ethers.providers.Provider;
+  contract: ethers.Contract;
+
+  constructor(private configService: ConfigService) {
+    const apiKey = this.configService.get<string>('INFURA_API_KEY');
+    this.provider = new ethers.providers.InfuraProvider('sepolia', apiKey); // Instantiate InfuraProvider using the 'new' keyword
+    this.contract = new ethers.Contract(
+      tokenJson.contractAddress,
+      tokenJson.abi,
+      this.provider
+    );
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getLastBlock() : Promise<ethers.providers.Block> {
+    // const provider = ethers.getDefaultProvider('sepolia');
+    return this.provider.getBlock("latest");
+  }
+
+  getContractAddress() {
+    const contractAddressToken = this.configService.get<string>('TOKEN_ADDRESS')
+    return contractAddressToken
+  }
+
+  getTotalSupply() {
+    return this.contract.getContractAddress().balanceof();
+  }
+
+  getBalance(address: string) {
+    return this.contract.getContractAddress().balanceOf(address);
+  }
+
+  async getReceipt(hash: string) {
+    const tx = await this.provider.getTransaction(hash);
+    const receipt = await tx.wait();
+    return receipt;
+  }
+
+  async awaitTx(tx: ethers.providers.TransactionResponse)
+    {
+      return await tx.wait();
+    }
+
+  requestTokens(address: string) {
+
+    const pKey = this.configService.get<string>('PRIVATE_KEY');
+
+    const wallet = new ethers.Wallet(pKey);
+
+    const signer = wallet.connect(this.provider);
+    
+    return this.contract.mint(address, ethers.utils.parseUnits("1"));
+  
+  }
+
+}
